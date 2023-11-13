264b7ea078c25c597d5294b32590a2b7d83fcab3
Ruslan <mnogoludey2@gmail.com>
2023-11-13 14:10:32 +0400

    finished the game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Die from './Die';
+import Confetti from 'react-confetti';
 import './style.css';
 
 export default function App () {
   const [dice, setDice] = React.useState(allNewDice());
+  const [win, setWin] = React.useState(false);
+  let [countRoll, setCountRoll] = React.useState(0);
+
+  useEffect(() => {
+    const dieValue = dice[0].value;
+    const allHeld = dice.every(die => die.held);
+    const allSameValue = dice.every(die => die.value === dieValue);
+
+    if (allHeld && allSameValue) {
+        setWin(true);
+        setCountRoll(0);
+    } else {
+        setCountRoll(prevCount => prevCount + 1);
+    }
+  }, [dice]); 
+
+  function holdDice(id) {
+    const newDice = dice.map((die) =>
+        die.id === id ?
+        {...die, held: !die.held} :
+        die
+    )
+    setDice(newDice);
+  }
+
+  function rollDice() {
+    if (!win) {
+            const newDice = dice.map((die, ind) => 
+            die.held ?
+            die :
+            {value: randomDieValue(), held: false, id: ind+1}
+          )
+        setDice(newDice);
+    } else {
+        setDice(allNewDice);
+        setWin(false);
+    }
+  }
 
   function randomDieValue() {
     return Math.ceil(Math.random() * 6);
@@ -23,17 +62,19 @@ export default function App () {
   }
 
   const diceElements = dice.map((die) => (
-    <Die key={die.id} {...die} hold={() => console.log('holded')} />
+    <Die key={die.id} {...die} hold={() => holdDice(die.id)} />
   ))
 
   return (
     <main>
+        {win && <Confetti />}
       <h1>Tenzies</h1>
       <p>Roll until all dice are the same. 
         Click each die to freeze it at its current value between rolls.</p>
       <div className='die-container'>{diceElements}</div>
-        <button className='roll-dice'>
-          Roll
+        <p>{`Your count of roll: ${countRoll}`}</p>
+        <button className='roll-dice' onClick={rollDice}>
+          {win ? 'Reset Game' : 'Roll'}
         </button>
     </main>
   )
--- src/Die.js
+++ src/Die.js
@@ -4,7 +4,7 @@ export default function Die(props) {
     const background = props.held ? 'held' : '';
 
     return (
-        <div className={`die-face ${background}`} onClick={() => console.log('clicked')}>
+        <div className={`die-face ${background}`} onClick={props.hold}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
